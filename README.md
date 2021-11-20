4d8f075401c305c0913e25f868c9d21dc1ba79e1
kasai <kasaipreston@yahoo.com>
2021-11-19 21:11:22 -0800

    ReadMe and comments

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Password-Generator-
-pw gen
+# Password-Generator
+
+## Outline
+For this project I used Javascript to build a password generator.
+
+## The goal
+Build a password generator that has a fixed number of characters between 8 and 128 no more, no less. It Prompts the user to include or exclude  uppercase, lowercase, numbers, and or special characters to be used in the generated password. 
+
+## The solution
+I acheived this by storing strings in varibles that were split to be choosen indiviually with the "concat" method. Too prompt the user for input i used "if" statements that contain booleans and operators to compare them to one another. I also used a "for loop" with the "math.floor" and "math.random" methods to drill into the no longer empty array. In turn this will grab a random character from the split "strings" and put them into another string which results in a randomly generatord password.
+
+## Preview
+Have a look.
+
+GitHub Pages: 
+GitHub Repository: 
+
+![Screenshot](assets/images/screenshpt.png)
--- script.js
+++ script.js
@@ -2,72 +2,60 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
-
-
-
 function generatePassword() {
-  var upperCase = ["ABCDEFGHILMNOPQRSTUVWXYZ"];
-  var lowerCase = ["abcdefghilmnopqrstuvwxyz"];
-  var specialChar = ["!@#$%^&*"];
-  var numberChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  // variables with with split strings
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var specialChar = '!@#$%^&*'.split('');
+  var numberChar = '0123456789'.split('');
 
-  var passwordLength = 8;
-  var upperCaseLean = false;
-  var lowerCaseLean = false;
-  var specialCharLean = false;
-  var numberCharLean = false;
+  // True False var's for if statements 
+  var passwordLength;
+  var upperCaseLean;
+  var lowerCaseLean;
+  var specialCharLean;
+  var numberCharLean;
+  // empty array to merge string varibles into
   var desiredCharacter = [];
-
+  // empty string for password
   var password = "";
 
+  // prompts and alerts
+
   alert("Select password");
-  var passwordLength = prompt("Please Select password length between 8 and 128 characters");
+  var passwordLength = parseInt(prompt("Please Select password length between 8 and 128 characters"));
 
   if (passwordLength < 8 || passwordLength > 128) {
-    alert(" Invalid input. ")
-    generatePassword()
+    alert(" Invalid input. ");
+    return;
   }
-  if (confirm("Do you want to include lowercase characters? ")) {
-    desiredCharacter.push(lowerCaseLean);
+  if (lowerCaseLean = confirm("Do you want to include lowercase characters? ")) {
+    desiredCharacter = desiredCharacter.concat(lowerCase);
   }
-  if (confirm(" Do you want to include uppercase characters? ")) {
-    desiredCharacter.push(upperCaseLean);
+  if (upperCaseLean = confirm(" Do you want to include uppercase characters? ")) {
+    desiredCharacter = desiredCharacter.concat(upperCase);
   }
-  if (confirm("Do you want to include numbers? ")) {
-    desiredCharacter.push(numberCharLean);
+  if (numberCharLean = confirm("Do you want to include numbers? ")) {
+    desiredCharacter = desiredCharacter.concat(numberChar);
   }
-  if (confirm(" Do you want special characters? ")) {
-    desiredCharacter.push(specialCharLean);
+  if (specialCharLean = confirm(" Do you want special characters? ")) {
+    desiredCharacter = desiredCharacter.concat(specialChar);
   }
-  if (desiredCharacter.length === 0) {
-    alert("Minimum of one type of character must be chosen");
-    generatePassword()
+  if ((desiredCharacter.length === 0)) {
+    alert("Invalid input please try again.");
+    return;
+
+    // for loop to pick random propertie from split string
   }
   for (let i = 0; i < passwordLength; ++i) {
     let random = Math.floor(Math.random() * Math.floor(desiredCharacter.length));
     password += desiredCharacter[random];
 
   }
-
   return password;
 }
 
-
-
-
 console.log('generate a password.');
-
-
-
-
-function getRandom() {
-  var randomChar = math.random() * 10;
-  var roundedChar = math.floor(randomChar);
-}
-
-
-
 
 function writePassword() {
   var password = generatePassword();
@@ -76,8 +64,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
 generateBtn.addEventListener("click", writePassword);
 
 
